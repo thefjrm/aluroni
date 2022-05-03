1d5e7abe6adbff3a256057f563459c14ac36c50c
thefjrm <felipemartinsplayernet@gmail.com>
2022-05-03 16:55:32 -0300

    Criação do Header

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -7,6 +7,12 @@ export default function Cardapio() {
       <nav className={style.menu}>
         <Logo />
       </nav>
+      <header className={style.header}>
+        <div className={style.header__text}>A casa do código e da massa.</div>
+      </header>
+      <section className={style.cardapio}>
+        <h3 className={style.cardapio__titulo}>Cardápio</h3>
+      </section>
     </main>
   );
 }
